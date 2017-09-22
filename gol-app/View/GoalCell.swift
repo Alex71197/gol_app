0f7a64d2c51d2e984e070360d7430167955be936
Alex Villacres <alex@Alexs-MacBook-Pro.local>
2017-09-22 13:05:19 -0700

    Added custom cell class

---
+++ gol-app/View/GoalCell.swift
@@ -0,0 +1,35 @@
+//
+//  GoalCell.swift
+//  gol-app
+//
+//  Created by Alex Villacres on 9/22/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+
+    }
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+
+}
